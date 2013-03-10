4c0b7ad52bea19000068214da8bb098dfb8f4bb2
Greg V <hmaon@bumba.net>
2013-03-09 23:38:11 -0500

    ...

--- src/Battlescape/AggroBAIState.h
+++ src/Battlescape/AggroBAIState.h
@@ -43,7 +43,6 @@ protected:
 	static std::vector<Position> _randomTileSearch;
 	static int _randomTileSearchAge;
 	bool _charge, _traceAI;
-	Position _lastCover;
 	
 public:
 	/// Creates a new AggroBAIState linked to the game and a certain unit.
